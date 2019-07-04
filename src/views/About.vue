454fb1fa49f5021d9645ba45c4689db68a49a3f1
thenewadeel <thenewadeel@gmail.com>
2019-07-04 12:32:48 +0500

    changed about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,32 @@
 <template>
   <v-container fluid>
     <div class="about">
-      <h1>Like Us on Facebook!</h1>
-      <a href="https://www.facebook.com/WittnessTech/">WittnessTech on Facebook</a>
-      <br>
-      <a href="https://github.com/wittnesstech/qarisaab">
-        <v-icon>fab fa-github-alt</v-icon>
-      </a>
+      <v-card>
+        <h2 class="headline mb-0">Mission Statement</h2>
+        <!-- <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img>-->
+
+        <v-card-title primary-title>
+          <v-layout column justify-space-around>
+          <p>Our mission is to teach quran through smart phones and computers. </p>
+          <p>You will not need any kindof teacher once our platform is released in the next Ramzan.</p>
+<p> Insha-Allah</p>
+</v-layout>
+          <!-- <div> {{ card_text }} </div> -->
+        </v-card-title>
+
+        <v-card-actions>
+          <!-- <v-btn flat color="orange">Share</v-btn> -->
+          <!-- <v-btn flat color="orange">Explore</v-btn> -->
+          <v-layout row justify-space-around>
+            <a href="https://www.facebook.com/WittnessTech/">Like Us on Facebook!</a>
+            <!-- <v-icon></v-icon> -->
+            <a href="https://github.com/wittnesstech/qarisaab">Github</a>
+          </v-layout>
+        </v-card-actions>
+      </v-card>
     </div>
   </v-container>
 </template>
